7757568f4898af0b108301107959e14604830c34
playdohface <playdohface@gmail.com>
2023-01-21 12:53:18 +0100

    add file serving and some test files

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,43 @@
+use rocket::form::Form;
+use rocket::fs::NamedFile;
+use std::path::{Path, PathBuf};
+
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, let's test this."
+fn index() -> String {
+    "Hello, let's test this even more.".to_string()
+}
+
+#[get("/hello")]
+fn hello() -> String {
+    "Why hello to you too, sir!".to_string()
+}
+
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).await.ok()
+}
+
+
+
+
+#[derive(FromForm)]
+struct Msg<'r> {
+    name: &'r str,
+    email: &'r str,
+    message: &'r str,
+}
+
+#[post("/msg", format = "form",  data = "<formdata>")]
+fn msg(formdata: Form<Msg<'_>>) -> String {
+    format!("Message submitted! Your name: {}, your E-Mail: {} and your Message: {}", formdata.name, formdata.email, formdata.message)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, hello, files, msg])
+
 }
